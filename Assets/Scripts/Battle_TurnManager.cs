004e7de9bbc1a289f6c6e82535538d77b8ac6729
SonKatarina23 <37500390+SonKatarina23@users.noreply.github.com>
2019-02-18 14:48:06 +0700

    s

--- Assets/Scripts/Battle_TurnManager.cs
+++ Assets/Scripts/Battle_TurnManager.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class Battle_TurnManager : MonoBehaviour
 {
+    public static Battle_TurnManager Instance;
+    public enum e_TurnState {
+        Start,
+        DicePhase,
+        BattlePhase,
+        EnemyPhase,
+        EndPhase
+    }
+    public e_TurnState m_TurnState;
 
+    private void Awake() {
+        f_Singleton();
+
+    }
+
+    public void f_Singleton() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void f_NextPhase() {
+
+        if (m_TurnState == e_TurnState.Start) m_TurnState = e_TurnState.DicePhase;
+        else if (m_TurnState == e_TurnState.DicePhase) m_TurnState = e_TurnState.BattlePhase;
+        else if (m_TurnState == e_TurnState.BattlePhase) m_TurnState = e_TurnState.EnemyPhase;
+        else if (m_TurnState == e_TurnState.EnemyPhase) m_TurnState = e_TurnState.EndPhase;
+        else if (m_TurnState == e_TurnState.EndPhase) m_TurnState = e_TurnState.Start;
+
+    }
+
+    private void Update() {
+        f_Update();
+    }
+
+    void f_Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            f_NextPhase();
+        }
+    }
 }
