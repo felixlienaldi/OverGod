eed1e84815c8026530d755b2ba97b7a7265712a8
phazeless <felixlienaldi@gmail.com>
2019-02-18 14:41:10 +0700

    qwq

--- Assets/Scripts/Class_Unit.cs
+++ Assets/Scripts/Class_Unit.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Class_Unit : MonoBehaviour
-{
+[System.Serializable]
+public class Class_Unit{
+    public int m_HealthPoint;
+    public int m_PhysicalAttack;
+    public int m_MagicAttack;
+    public int m_Defense;
+    public int m_Resistance;
+    public int m_CriticalDamage;
+    public int m_CriticalChance;
+    public Class_Skill c_Skill = new Class_Skill();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
---
+++ Assets/Scripts/Unit_Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit_Character : MonoBehaviour
+{
+    public int m_AttributePoint;
+    public Class_Unit m_Unit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
